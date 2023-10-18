6dbb9b512d0176d7ec6c2b37bc9dd1346720f907
Ricardo <ricardo.ferraz@medportal.com>
2023-10-18 09:52:50 -0300

    Booleanos

---
+++ 3_Tipos_de_dados/10_booleanos/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    echo true;
+    echo false;
+    echo "<br>";
+
+    if(true) {
+        echo "É verdadeiro <br>";
+    }
+
+    if(5 > 2) {  // true
+       echo "É verdadeiro! <br>";
+    
+    }
+
+?>
