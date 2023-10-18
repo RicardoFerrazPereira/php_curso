146cb20ad47451aca650d962a591f0b7d876e988
Ricardo <ricardo.ferraz@medportal.com>
2023-10-18 12:18:39 -0300

    Null

---
+++ 3_Tipos_de_dados/17_null/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo NULL;
+
+    $nome = NULL;
+
+    if(is_null($nome)) {
+        echo "O valor é nulo";
+    }
+
+    $nome = "Ziva";
+
+    if(is_null($nome)) {
+        echo "O valor é nulo 2";
+    }
+
+
+
+?>
