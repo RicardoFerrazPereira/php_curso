58a1271007af6bf1dd17b237691c3fda1554a228
Ricardo <ricardo.ferraz@medportal.com>
2023-10-18 09:52:18 -0300

    Exercicio 5 Strings

---
+++ 3_Tipos_de_dados/9_exercicio_5/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "Vasco da 'Gama'<br>";
+    echo 'Vasco da "Gama"<br>';
+
+    $nome = "Edddie";
+    $idade = 10;
+
+    echo "Olá, eu sou o $nome e tenho $idade anos<br>";
+    echo 'Olá, eu sou o $nome e tenho $idade anos'; // Aspas simples não interpreta a variável
+
+?>
