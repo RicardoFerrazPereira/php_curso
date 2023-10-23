832f8a4cc594dfe9a761e60144c67ccccc5434bc
Ricardo <ricardo.ferraz@medportal.com>
2023-10-23 15:17:40 -0300

    Variável de variável

---
+++ 4_Variaveis/4_var_de_var/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    $var = "nome"; // valor de nome
+
+    echo "$var <br>";
+
+    $$var = "Vasco"; // o valor da variavel var vai virar o nome de variavel com o valor Vasco
+
+    echo $nome;
