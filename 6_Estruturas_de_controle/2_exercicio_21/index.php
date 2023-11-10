81c0ccedc10278f92bf8c9ad8e4e0945bcf56585
Ricardo <ricardo.ferraz@medportal.com>
2023-11-10 00:26:08 -0300

    Exercício if

---
+++ 6_Estruturas_de_controle/2_exercicio_21/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    if(5 > 2) {
+        echo "5 é maior que 2 <br>";    
+    }
+
+    $nome = "Farroch";
+    $nome2 = "Bulsara";
+
+    if($nome != $nome2) {
+        echo "Nomes diferentes <br>";
+    
+    }
+
+    $num1 = 12;
+    $num2 = 11;
+
+    if($num1 <= $num2) {
+        echo "Num1 é maior que num2 <br>";
+    
+    }
