14a72f8750be7ceb13ecb494e50555fb360e5da1
Ricardo <ricardo.ferraz@medportal.com>
2023-11-09 22:39:38 -0300

    IF

---
+++ 6_Estruturas_de_controle/1_if/index.php
@@ -0,0 +1,29 @@
+<?php
+    // condição verdadeira
+   if(10 > 2) {
+    echo "True! Entrou no IF <br>";
+   } 
+
+   // condição falsa
+   if(5 < 3) {
+    echo "Não vai entrar no if, porque deu FALSE";
+   }
+
+   // utilizar op. lógico
+  if(10 === 10 && 9 > 3) {
+
+    echo "Deu certo! Entrou no if 2 <br>";
+
+  }
+
+  // variáveis
+  $a = 10;
+  $b = 5;
+
+  $c = "Deu certo, entrou no if 3 <br>";
+
+  if($a >= $b) {
+
+    echo $c;
+
+  }
